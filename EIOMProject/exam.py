c6ccc7f7bb3f4e5b2854e17f2dba7d681f92b008
lyj310 <yoenjii310@gmail.com>
2020-11-30 01:27:19 +0900

    개같은 그래프

---
+++ EIOMProject/exam.py
@@ -0,0 +1,161 @@
+
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PySide2.QtCore import QRect
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2 import QtCore
+
+from BasicInfo import BasicInfo
+
+
+class noticeList(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.basicInfo = BasicInfo()
+        self.w = QWidget(self)
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.setWindowTitle('EIOM')
+        self.w.resize(self.basicInfo.WindowWidth, self.basicInfo.WindowHeight)
+        self.move(self.basicInfo.WindowX, self.basicInfo.WindowY)
+        self.setFixedSize(self.basicInfo.WindowWidth, self.basicInfo.WindowHeight)
+
+        palette = QPalette()
+        palette.setBrush(QPalette.Background, QBrush(QPixmap("img/background.png")))
+        self.setPalette(palette)
+
+
+        title = QLabel("EIOM", self.w)
+        title.setFont(QFont(self.basicInfo.titleFont, 25))
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        title.setGeometry(100, 10, 1000, 50)
+
+
+
+
+        stateBtn = QPushButton('통계', self.w)
+        stateBtn.setFont(QFont(self.basicInfo.font1, 13))
+        stateBtn.setGeometry(0, 70, 200, 50)
+        stateBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        noticeBtn = QPushButton('공지', self.w)
+        noticeBtn.setFont(QFont(self.basicInfo.font1, 13))
+        noticeBtn.setGeometry(200, 70, 200, 50)
+        noticeBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        companyBtn = QPushButton('회사', self.w)
+        companyBtn.setFont(QFont(self.basicInfo.font1, 13))
+        companyBtn.setGeometry(400, 70, 200, 50)
+        companyBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        postBtn = QPushButton('취업의뢰', self.w)
+        postBtn.setFont(QFont(self.basicInfo.font1, 13))
+        postBtn.setGeometry(600, 70, 200, 50)
+        postBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        pfBtn = QPushButton('포트폴리오', self.w)
+        pfBtn.setFont(QFont(self.basicInfo.font1, 13))
+        pfBtn.setGeometry(800, 70, 200, 50)
+        pfBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        infoBtn = QPushButton('내 정보', self.w)
+        infoBtn.setFont(QFont(self.basicInfo.font1, 13))
+        infoBtn.setGeometry(1000, 70, 200, 50)
+        infoBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+
+
+        #
+        layout = QVBoxLayout()
+        self.w.setLayout(layout)
+        emptyLabel = QLabel(" ")
+        emptyLabel.setGeometry(500, 500, 1000, 50)
+        layout.addWidget(emptyLabel)
+
+        N = 4
+        value = (32, 15, 14, 14)
+        ind = np.arange(N)
+        width = 0.2
+        fig = plt.Figure()
+        canvas = FigureCanvas(fig)
+        ax = fig.add_subplot()
+        ax.bar(ind, value, width, color='lightblue')
+        ax.set_xticks(ind + width / 20)
+        ax.set_xticklabels(['Java', 'C', 'javascript', 'Spring'])
+
+        layout.addWidget(canvas)
+        #
+
+
+        self.show()
+'''
+        
+        palette = QPalette()
+        palette.setBrush(QPalette.Background, QBrush(QPixmap("../img/background.png")))
+        self.setPalette(palette)
+
+        title = QLabel("EIOM", self.w)
+        title.setFont(QFont(self.basicInfo.titleFont, 25))
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        layout.addWidget(title)
+        title.setGeometry(100, 10, 1000, 50)
+
+
+
+        stateBtn = QPushButton('통계', self.w)
+        stateBtn.setFont(QFont(self.basicInfo.font1, 13))
+        stateBtn.setGeometry(0, 70, 200, 50)
+        stateBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        noticeBtn = QPushButton('공지', self.w)
+        noticeBtn.setFont(QFont(self.basicInfo.font1, 13))
+        noticeBtn.setGeometry(200, 70, 200, 50)
+        noticeBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        companyBtn = QPushButton('회사', self.w)
+        companyBtn.setFont(QFont(self.basicInfo.font1, 13))
+        companyBtn.setGeometry(400, 70, 200, 50)
+        companyBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        postBtn = QPushButton('취업의뢰', self.w)
+        postBtn.setFont(QFont(self.basicInfo.font1, 13))
+        postBtn.setGeometry(600, 70, 200, 50)
+        postBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        pfBtn = QPushButton('포트폴리오', self.w)
+        pfBtn.setFont(QFont(self.basicInfo.font1, 13))
+        pfBtn.setGeometry(800, 70, 200, 50)
+        pfBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        infoBtn = QPushButton('내 정보', self.w)
+        infoBtn.setFont(QFont(self.basicInfo.font1, 13))
+        infoBtn.setGeometry(1000, 70, 200, 50)
+        infoBtn.setStyleSheet('background-color: rgb(255,255,255); border:1px solid lightgray; ')
+
+        self.show()
+
+'''
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    ex = noticeList()
+    sys.exit(app.exec_())
+
