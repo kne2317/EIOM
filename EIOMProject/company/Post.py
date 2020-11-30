3937a11e7e7ed96890f1c44082166d01ba66a58d
kne2317 <skdms2317@gmail.com>
2020-12-01 03:13:11 +0900

    asdf

--- EIOMProject/company/Post.py
+++ EIOMProject/company/Post.py
@@ -2,8 +2,9 @@ import sys
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2 import QtCore
-from BasicInfo import BasicInfo
-
+from BasicInfo import BasicInfo, BasicDB
+import company.NoneEmployementRequest
+import company.Company
 
 class Post(QWidget):
 
@@ -79,41 +80,43 @@ class Post(QWidget):
         for i in range(0, 30):
             day2.addItem(str(i + 1) + "일")
 
+        self.recruit=year1.text()+"년"+month1.currentText()+day1.currentText()+" ~ "+year2.text()+"년"+month2.currentText()+day2.currentText();
+
         la3 = QLabel('희망 인원', self.w)
         la3.setFont(QFont(self.basicInfo.font1, 12))
         la3.setGeometry(100, 210, 100, 40)
 
-        hopePerson = QLineEdit(self.w)
-        hopePerson.setFont(QFont(self.basicInfo.font1,13))
-        hopePerson.setGeometry(250,210,800,40)
-        hopePerson.setPlaceholderText('명')
+        self.hopePerson = QLineEdit(self.w)
+        self.hopePerson.setFont(QFont(self.basicInfo.font1,13))
+        self.hopePerson.setGeometry(250,210,800,40)
+        self.hopePerson.setPlaceholderText('명')
 
         la4 = QLabel('지원 요건', self.w)
         la4.setFont(QFont(self.basicInfo.font1, 12))
         la4.setGeometry(100, 270, 100, 40)
 
-        right = QLineEdit(self.w)
-        right.setFont(QFont(self.basicInfo.font1, 13))
-        right.setGeometry(250, 270, 800, 40)
-        right.setPlaceholderText('ex) python 가능자')
+        self.right = QLineEdit(self.w)
+        self.right.setFont(QFont(self.basicInfo.font1, 13))
+        self.right.setGeometry(250, 270, 800, 40)
+        self.right.setPlaceholderText('ex) python 가능자')
 
         la5 = QLabel('우대 사항', self.w)
         la5.setFont(QFont(self.basicInfo.font1, 12))
         la5.setGeometry(100, 330, 100, 40)
 
-        royalty = QLineEdit(self.w)
-        royalty.setFont(QFont(self.basicInfo.font1, 13))
-        royalty.setGeometry(250, 330, 800, 40)
-        royalty.setPlaceholderText('ex) cospro 자격증')
+        self.royalty = QLineEdit(self.w)
+        self.royalty.setFont(QFont(self.basicInfo.font1, 13))
+        self.royalty.setGeometry(250, 330, 800, 40)
+        self.royalty.setPlaceholderText('ex) cospro 자격증')
 
         la6 = QLabel('제출 서류', self.w)
         la6.setFont(QFont(self.basicInfo.font1, 12))
         la6.setGeometry(100, 390, 100, 40)
 
-        document = QLineEdit(self.w)
-        document.setFont(QFont(self.basicInfo.font1, 13))
-        document.setGeometry(250, 390, 800, 40)
-        document.setPlaceholderText('ex) 이력서, 포트폴리오, 자기소개서')
+        self.document = QLineEdit(self.w)
+        self.document.setFont(QFont(self.basicInfo.font1, 13))
+        self.document.setGeometry(250, 390, 800, 40)
+        self.document.setPlaceholderText('ex) 이력서, 포트폴리오, 자기소개서')
 
         la7 = QLabel('근무 형태', self.w)
         la7.setFont(QFont(self.basicInfo.font1, 15))
@@ -123,55 +126,55 @@ class Post(QWidget):
         la8.setFont(QFont(self.basicInfo.font1, 12))
         la8.setGeometry(100, 570, 100, 40)
 
-        useLang = QLineEdit(self.w)
-        useLang.setFont(QFont(self.basicInfo.font1, 13))
-        useLang.setGeometry(250, 570, 800, 40)
-        useLang.setPlaceholderText('ex) python, node.js ( , 로 구분 )')
+        self.useLang = QLineEdit(self.w)
+        self.useLang.setFont(QFont(self.basicInfo.font1, 13))
+        self.useLang.setGeometry(250, 570, 800, 40)
+        self.useLang.setPlaceholderText('ex) python, node.js ( , 로 구분 )')
 
         la9 = QLabel('고용 형태', self.w)
         la9.setFont(QFont(self.basicInfo.font1, 12))
         la9.setGeometry(100, 630, 100, 40)
 
-        employment = QLineEdit(self.w)
-        employment.setFont(QFont(self.basicInfo.font1, 13))
-        employment.setGeometry(250, 630, 800, 40)
-        employment.setPlaceholderText('ex) 현장실습 후 정규직 채용')
+        self.employment = QLineEdit(self.w)
+        self.employment.setFont(QFont(self.basicInfo.font1, 13))
+        self.employment.setGeometry(250, 630, 800, 40)
+        self.employment.setPlaceholderText('ex) 현장실습 후 정규직 채용')
 
         la10 = QLabel('담당 업무', self.w)
         la10.setFont(QFont(self.basicInfo.font1, 12))
         la10 .setGeometry(100, 690, 100, 40)
 
-        work = QLineEdit(self.w)
-        work.setFont(QFont(self.basicInfo.font1, 13))
-        work.setGeometry(250, 690, 800, 40)
-        work.setPlaceholderText('ex) 백엔드 개발')
+        self.work = QLineEdit(self.w)
+        self.work.setFont(QFont(self.basicInfo.font1, 13))
+        self.work.setGeometry(250, 690, 800, 40)
+        self.work.setPlaceholderText('ex) 백엔드 개발')
 
         la11 = QLabel('업무 시간', self.w)
         la11.setFont(QFont(self.basicInfo.font1, 12))
         la11.setGeometry(100, 750, 100, 40)
 
-        workTime = QLineEdit(self.w)
-        workTime.setFont(QFont(self.basicInfo.font1, 13))
-        workTime.setGeometry(250, 750, 800, 40)
-        workTime.setPlaceholderText('ex) 주 40시간 근무')
+        self.workTime = QLineEdit(self.w)
+        self.workTime.setFont(QFont(self.basicInfo.font1, 13))
+        self.workTime.setGeometry(250, 750, 800, 40)
+        self.workTime.setPlaceholderText('ex) 주 40시간 근무')
 
         la12 = QLabel('급여', self.w)
         la12.setFont(QFont(self.basicInfo.font1, 12))
         la12.setGeometry(100, 810, 100, 40)
 
-        money = QLineEdit(self.w)
-        money.setFont(QFont(self.basicInfo.font1, 13))
-        money.setGeometry(250, 810, 800, 40)
-        money.setPlaceholderText('ex) 연봉 2400만원')
+        self.money = QLineEdit(self.w)
+        self.money.setFont(QFont(self.basicInfo.font1, 13))
+        self.money.setGeometry(250, 810, 800, 40)
+        self.money.setPlaceholderText('ex) 연봉 2400만원')
 
         la13 = QLabel('복리후생', self.w)
         la13.setFont(QFont(self.basicInfo.font1, 12))
         la13.setGeometry(100, 870, 100, 40)
 
-        benefit = QLineEdit(self.w)
-        benefit.setFont(QFont(self.basicInfo.font1, 13))
-        benefit.setGeometry(250, 870, 800, 40)
-        benefit.setPlaceholderText('ex) 중식, 간식, 석식 제공')
+        self.benefit = QLineEdit(self.w)
+        self.benefit.setFont(QFont(self.basicInfo.font1, 13))
+        self.benefit.setGeometry(250, 870, 800, 40)
+        self.benefit.setPlaceholderText('ex) 중식, 간식, 석식 제공')
 
         la14 = QLabel('현장 실습 정보', self.w)
         la14.setFont(QFont(self.basicInfo.font1, 15))
@@ -181,28 +184,19 @@ class Post(QWidget):
         la15.setFont(QFont(self.basicInfo.font1, 12))
         la15.setGeometry(100, 1050, 200, 40)
 
-        period = QLineEdit(self.w)
-        period.setFont(QFont(self.basicInfo.font1, 13))
-        period.setGeometry(250, 810, 800, 40)
-        period.setPlaceholderText('ex) 2개월')
-
-        la16 = QLabel('현장 실습 기간', self.w)
-        la16.setFont(QFont(self.basicInfo.font1, 12))
-        la16.setGeometry(100, 1050, 150, 40)
-
-        period = QLineEdit(self.w)
-        period.setFont(QFont(self.basicInfo.font1, 13))
-        period.setGeometry(250, 1050, 800, 40)
-        period.setPlaceholderText('ex) 2개월')
+        self.period = QLineEdit(self.w)
+        self.period.setFont(QFont(self.basicInfo.font1, 13))
+        self.period.setGeometry(250, 810, 800, 40)
+        self.period.setPlaceholderText('ex) 2개월')
 
         la17 = QLabel('현장 실습 수당', self.w)
         la17.setFont(QFont(self.basicInfo.font1, 12))
         la17.setGeometry(100, 1110, 150, 40)
 
-        pmoney = QLineEdit(self.w)
-        pmoney.setFont(QFont(self.basicInfo.font1, 13))
-        pmoney.setGeometry(250, 1110, 800, 40)
-        pmoney.setPlaceholderText('ex) 월 150만원')
+        self.pmoney = QLineEdit(self.w)
+        self.pmoney.setFont(QFont(self.basicInfo.font1, 13))
+        self.pmoney.setGeometry(250, 1110, 800, 40)
+        self.pmoney.setPlaceholderText('ex) 월 150만원')
 
         la18 = QLabel('담당자 정보', self.w)
         la18.setFont(QFont(self.basicInfo.font1, 15))
@@ -212,25 +206,30 @@ class Post(QWidget):
         la19.setFont(QFont(self.basicInfo.font1, 12))
         la19.setGeometry(100, 1290, 150, 40)
 
-        email = QLineEdit(self.w)
-        email.setFont(QFont(self.basicInfo.font1, 13))
-        email.setGeometry(250, 1290, 800, 40)
-        email.setPlaceholderText('ex) mirim@e-mirim.hs.kr')
+        self.email = QLineEdit(self.w)
+        self.email.setFont(QFont(self.basicInfo.font1, 13))
+        self.email.setGeometry(250, 1290, 800, 40)
+        self.email.setPlaceholderText('ex) mirim@e-mirim.hs.kr')
 
         la20 = QLabel('전화번호', self.w)
         la20.setFont(QFont(self.basicInfo.font1, 12))
         la20.setGeometry(100, 1350, 150, 40)
 
-        ph = QLineEdit(self.w)
-        ph.setFont(QFont(self.basicInfo.font1, 13))
-        ph.setGeometry(250, 1350, 800, 40)
-        ph.setPlaceholderText('ex) 01012341234')
+        self.ph = QLineEdit(self.w)
+        self.ph.setFont(QFont(self.basicInfo.font1, 13))
+        self.ph.setGeometry(250, 1350, 800, 40)
+        self.ph.setPlaceholderText('ex) 01012341234')
 
         requestBtn = QPushButton('확인', self.w)
         requestBtn.setFont(QFont(self.basicInfo.font1, 12))
-        requestBtn.setGeometry(930, 1410, 120, 40)
+        requestBtn.setGeometry(800, 1410, 120, 40)
         requestBtn.setStyleSheet('background-color: white; border:1px solid lightgray;')
 
+        cancleBtn = QPushButton('취소', self.w)
+        cancleBtn.setFont(QFont(self.basicInfo.font1, 12))
+        cancleBtn.setGeometry(930, 1410, 120, 40)
+        cancleBtn.setStyleSheet('background-color: white; border:1px solid lightgray;')
+        cancleBtn.clicked.connect(self.cancle)
 
         scroll = QScrollArea()
         scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
@@ -242,11 +241,33 @@ class Post(QWidget):
         vLayout.addWidget(scroll)
         vLayout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(vLayout)
-
+    def requestBtn(self):
+        insertPost(company.Company.Company.ID, self.period, self.hopePerson, self.right, self.royalty,self.document,
+                   self.useLang,self.employment,self.work,self.money,self.workTime,self.benefit,self.period,
+                   self.pmoney,self.email,self.ph)
+        
+    def cancle(self):
+        self.none=company.NoneEmployementRequest.NoneEmployementRequest()
+        self.none.show()
+        self.hide()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     ex = Post()
-    sys.exit(app.exec_())
+    ex.show()
+    app.exec_()
+
+def insertPost(company_id, recruit, hopeperson, apply, royalty, document,
+               uselang, employment, work, money, worktime, benefit,period,
+               pmoney, manager_email, manager_ph):
+    basicDB = BasicDB()
+    conn = basicDB.conn
+    curs = conn.cursor()
+
+    sql = "insert into employment_request ( company_id, recruit, hopeperson, apply, " \
+          "royalty, document, uselang, employment, work, money, worktime, benefit, " \
+          "period, pmoney, manager_email, manager_ph) values ( '"+company_id+"', '"+recruit+"', "+hopeperson+", '"+\
+          apply+"','"+royalty+"', '"+document+"', '"+uselang+"', '"+employment+"', '"+work+"', '"+money+"', '"+\
+          worktime+"', '"+benefit+"', '"+period+"','"+ pmoney+"', '"+manager_email+"', '"+manager_ph+"');"
+    curs.execute(sql)
